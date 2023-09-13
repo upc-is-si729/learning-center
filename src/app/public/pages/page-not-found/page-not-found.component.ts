df60e3dc1939e420d0f6fe8377b3e5c4c8e135d6
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-12 21:42:24 -0500

    feat(page-not-found): added visualization elements and behavior for page not found component.

--- src/app/public/pages/page-not-found/page-not-found.component.ts
+++ src/app/public/pages/page-not-found/page-not-found.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
+  invalidUrl: string = '';
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+
+  }
+
+  navigateToHome() {
+    this.router.navigate(['home']).then();
+  }
+
+  ngOnInit(): void {
+    this.invalidUrl = this.route.snapshot.url[0].path;
+  }
+
 
 }
