cf38afbdcb731a91e4f3842a8c43523fe37f2fde
Angel Velasquez <pcisavel@upc.edu.pe>
2023-09-12 21:10:36 -0500

    feat(students): added in-app navigation route for students component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {StudentsComponent} from "./learning/pages/students/students.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'students', component: StudentsComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
